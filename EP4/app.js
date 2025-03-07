f8cd55fc0e87b4b7277092e3557e09e4a1277466
VedanshMaheshwari <vedanshm15@gmail.com>
2025-03-07 05:56:36 +0530

    EP4

--- EP4/app.js
+++ EP4/app.js
@@ -1,6 +1,5 @@
-import react from "react";
-import reactDOM from "react-dom";
-
+import React from "react";
+import ReactDOM from "react-dom/client"; 
 /** 
  * Header
  * -logo
@@ -16,10 +15,35 @@ import reactDOM from "react-dom";
 */
  
 
-const appLayout = () => {
+const Header = () => {
     return (
-        <div className ="app">
-            
+        <div className ="header" >
+            <div className ="logo-container">
+                <img src = "https://images-platform.99static.com/v84irzbNBd5aawXGKXfH4SEjcn0=/0x0:960x960/500x500/top/smart/99designs-contests-attachments/117/117132/attachment_117132760"></img>
+            </div>
+
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+
         </div>
     )
-}
+};
+
+const AppLayout = () => {
+    return (
+        <div className ="app">
+
+            <Header />
+        
+        </div>
+    )
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout/>);
