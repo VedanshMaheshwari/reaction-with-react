16b0a6bdec7507b9c9a2246a9e9b28cfe16df210
VedanshMaheshwari <vedanshm15@gmail.com>
2025-03-10 21:06:26 +0530

    swiggy

--- EP4/app.js
+++ EP4/app.js
@@ -35,8 +35,37 @@ const Header = () => {
     )
 };
  
+const styleCard = {
+    backgroundColor: "#f0f0f0",
+
+};
+
+
+//Inline CSS - Style takes JAVASCRIPT OBJECT:
+
+const RestaurantCard = () =>{
+    return (
+        <div className="res-card" style={{
+            backgroundColor: "#f0f0f0",
+        
+        }}>
+            <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/e0vvulfbahjxjz6k4uwi"> x</img>
+            <h3>Meghana Foods</h3>
+        </div>
+    )
+}
+
 const Body = () =>{
-    return
+    return(
+        <div className = "body">
+            
+            <div className="search">Search</div>
+
+            <div className="res-container">
+            <RestaurantCard /> 
+            </div>
+        </div>
+    )    
 }
 
 const AppLayout = () => {
