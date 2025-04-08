dd33e0c7822afd63921e2b87523661e72c52ca28
VedanshMaheshwari <vedanshm15@gmail.com>
2025-04-08 13:44:03 +0530

    git ignore

--- EP3/app.js
+++ EP3/app.js
@@ -1,51 +1,20 @@
 import React from "react";
-import ReactDOM from "react-dom/client"; 
+import ReactDOM from "react-dom/client"; // Correct import for createRoot
 
 
-//React.createElement => object =>HTMLelement(render)
+//React Element 
+//React.createElement => Object => HTMLElement(render);
 
-// const heading = React.createElement( "h1", { id : "heading"} , "EPISODE 3");
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-
-
-// Using JSX - is not HTML in JS
-
-const jsxHeading = () =>  <h1 id ="heading" className="head" tabIndex="5">Vedansh creating react using JSR lesgoo</h1>;
-
-
-//React Components - Functional and Class-Based Components 
-
-// 1. Functional Component
-
-const elem =   <span>React element inside React element</span>;
-
-
-const HeadingComponent  = (
-    <div> 
-    {elem}
-    <h1>Functional Freaking Component</h1>
-    </div>
-)
-
-  
-const Title = () => (
-    <h1 className="titleHead">
-        Renders title component inside the heading component</h1>
+const heading = React.createElement("h1",
+    {id:"heading"},
+    "Veds is here REPLACED"
 );
+ 
 
+const jsxHeading =<h1>Tag using JSR</h1>;
 
-const FuncHeading = () => (
-    <div>
-    {elem}
-    {HeadingComponent}
-    {Title()} 
-    <Title/>
-    <Title></Title>
-    <h1>Functional Component is Being Rendered</h1>
-    </div>
-);
-
+ 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<FuncHeading />);
+root.render(heading);  
