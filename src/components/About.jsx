4bc4f741ce565413f991c0e41023c3dba2a972c9
VedanshMaheshwari <vedanshm15@gmail.com>
2025-08-01 23:55:43 +0530

    EP8

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,11 @@
+import User from "../components/User"
 
 const About = () => {
     return(
         <div>
             <h1>About</h1>
             <h2>I am learning router</h2>
+            <User />
         </div>
     );
 };
---
+++ src/components/User.jsx
@@ -0,0 +1,11 @@
+const User = () => {
+    return (
+        <div className="user-card">
+            <h1>Vedansh Maheshwari</h1>
+            <h2>Location: Dehradun</h2>
+            <h2>Contact: @VedanshMaheshwari</h2>
+        </div>
+    )
+}
+
+export default User;
