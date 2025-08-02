4bb119b01edd7af7325708e795626c9aa97474c4
VedanshMaheshwari <vedanshm15@gmail.com>
2025-08-02 21:43:04 +0530

    EP8

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,12 @@
 import User from "../components/User"
-
+import UserClass from "./UserClass"
 const About = () => {
     return(
         <div>
             <h1>About</h1>
             <h2>I am learning router</h2>
             <User />
+            <UserClass />
         </div>
     );
 };
---
+++ src/components/UserClass.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+class UserClass extends React.Component{
+    render(){
+        return (
+            <div className="user-card">
+                <h1>Vedansh Maheshwari</h1>
+                <h2>Location: Dehradun</h2>
+                <h2>Contact: @VedanshMaheshwari</h2>
+            </div>
+        )
+    }
+}
+
+export default UserClass;
